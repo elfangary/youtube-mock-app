a6e9210b82db81d1e3f4ff81444770a0aa387d76
Brent <ahmed.elfangary@robustastudio.com>
2021-03-02 05:23:18 +0200

    refactor: remove unused boilerplate components

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="filters">
-    <select>
+    <select v-model="typeVal">
       <option
         v-for="type in filtersOptions.typeFiltersOptions"
         :value="type.value"
         :key="type.value"
+        :disabled="type.disabled"
         >{{ type.title }}</option
       >
     </select>
-    <select>
+    <select v-model="dateVal">
       <option
         v-for="date in filtersOptions.dateFiltersOptions"
         :value="date.value"
         :key="date.value"
+        :disabled="date.disabled"
         >{{ date.title }}</option
       >
     </select>
@@ -20,41 +22,27 @@
 </template>
 
 <script>
+import { TYPE_FILTERS_OPTIONS, DATE_FILTERS_OPTIONS } from "@/constants";
+
 export default {
   name: "Filters",
   data: function() {
     return {
+      typeVal: "",
+      dateVal: "",
       filtersOptions: {
-        typeFiltersOptions: [
-          {
-            title: "All",
-            value: "all"
-          },
-          {
-            title: "Channel",
-            value: "channel"
-          },
-          {
-            title: "Playlist",
-            value: "playlist"
-          }
-        ],
-        dateFiltersOptions: [
-          {
-            title: "Today",
-            value: "today"
-          },
-          {
-            title: "This Week",
-            value: "thisWeek"
-          },
-          {
-            title: "This Month",
-            value: "thisMonth"
-          }
-        ]
+        typeFiltersOptions: TYPE_FILTERS_OPTIONS,
+        dateFiltersOptions: DATE_FILTERS_OPTIONS
       }
     };
+  },
+  watch: {
+    typeVal() {
+      this.$emit("type-search", this.typeVal);
+    },
+    dateVal() {
+      this.$emit("date-search", this.dateVal);
+    }
   }
 };
 </script>
