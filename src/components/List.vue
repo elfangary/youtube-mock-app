382e1dd4a3c4f5a96ff94c8fa7cdcff84d743db0
Brent <ahmed.elfangary@robustastudio.com>
2021-03-02 05:21:03 +0200

    feat: create list & listItem components

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="list"></div>
+  <ul class="list">
+    <li v-for="(item, i) in list" :key="i">
+      <ListItem item="item" />
+    </li>
+  </ul>
 </template>
 
 <script>
+import ListItem from "@/components/ListItem.vue";
+
 export default {
   name: "List",
-  data: function() {
-    return {
-      list: []
-    };
+  components: { ListItem },
+  props: {
+    list: {
+      type: Array
+    }
   }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.list {
+  outline: 1px solid $gray;
+}
+</style>
