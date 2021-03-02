806abc2195d6f899b35d2d8f63f8742505cff543
Brent <ahmed.elfangary@robustastudio.com>
2021-03-02 02:43:25 +0200

    feat: filters component

---
+++ src/components/Filters.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="filters">
+    <select>
+      <option
+        v-for="type in filtersOptions.typeFiltersOptions"
+        :value="type.value"
+        :key="type.value"
+        >{{ type.title }}</option
+      >
+    </select>
+    <select>
+      <option
+        v-for="date in filtersOptions.dateFiltersOptions"
+        :value="date.value"
+        :key="date.value"
+        >{{ date.title }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filters",
+  data: function() {
+    return {
+      filtersOptions: {
+        typeFiltersOptions: [
+          {
+            title: "All",
+            value: "all"
+          },
+          {
+            title: "Channel",
+            value: "channel"
+          },
+          {
+            title: "Playlist",
+            value: "playlist"
+          }
+        ],
+        dateFiltersOptions: [
+          {
+            title: "Today",
+            value: "today"
+          },
+          {
+            title: "This Week",
+            value: "thisWeek"
+          },
+          {
+            title: "This Month",
+            value: "thisMonth"
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.filters {
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+
+  select {
+    height: 50px;
+    flex: 1;
+    padding: 10px;
+    color: $gray;
+    background: $white;
+    font-size: 18px;
+    font-weight: bold;
+
+    & + select {
+      margin-left: 15px;
+    }
+  }
+}
+</style>
