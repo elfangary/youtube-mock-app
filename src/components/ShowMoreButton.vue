205dcce2041d3e2980b2e3f7d39c863aa444ebbc
Brent <ahmed.elfangary@robustastudio.com>
2021-03-03 01:03:00 +0200

    feat: create showMoreButton component

---
+++ src/components/ShowMoreButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="show-more-button" @click="handleClick">Show More Items</button>
+</template>
+
+<script>
+export default {
+  name: "ShowMoreButton",
+  props: {
+    handleClick: {
+      type: Function
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.show-more-button {
+  color: $gray;
+  width: 100%;
+  font-size: 16px;
+  background: none;
+  border: 0;
+  border-top: 1px solid $lightGray;
+  padding: 20px 0;
+  cursor: pointer;
+}
+</style>
