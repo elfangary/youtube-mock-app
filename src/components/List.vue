c744fb7e84e393baf843257f2d55cd2315725d60
Brent <ahmed.elfangary@robustastudio.com>
2021-03-02 02:43:40 +0200

    feat: list component

---
+++ src/components/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="list"></div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data: function() {
+    return {
+      list: []
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
