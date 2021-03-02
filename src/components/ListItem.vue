a5ff58d7c23de51721b300fa8d03040de31d0933
Brent <ahmed.elfangary@robustastudio.com>
2021-03-02 05:21:03 +0200

    feat: create list & listItem components

---
+++ src/components/ListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list-item"></div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    item: {
+      type: Object
+    }
+  },
+  data: function() {
+    return {
+      isVideo: this.defineItemType() === "video",
+      isPlayList: this.defineItemType() === "playlist",
+      isChannel: this.defineItemType() === "channel"
+    };
+  },
+  methods: {
+    defineItemType() {
+      return this.item.id.kind.replace("youtube#", "");
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.list-item {
+  color: $primary;
+}
+</style>
