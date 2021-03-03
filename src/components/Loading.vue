893db0b6465534d55b10f9de14711a0b084dde12
Brent <ahmed.elfangary@robustastudio.com>
2021-03-03 02:00:10 +0200

    feat: create loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading">
+    <LoadingIcon width="30" height="30" />
+    <p class="loading__text">Loading</p>
+  </div>
+</template>
+
+<script>
+import LoadingIcon from "@/components/icons/Loading.vue";
+
+export default {
+  name: "Loading",
+  components: { LoadingIcon }
+};
+</script>
+
+<style scoped lang="scss">
+.loading {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &__text {
+    color: $gray;
+  }
+}
+</style>
