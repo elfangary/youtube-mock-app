65f0e148e6a8b31926dab0e0bef94da3670e514a
Brent <ahmed.elfangary@robustastudio.com>
2021-03-05 05:26:44 +0200

    feat: create container component

---
+++ src/components/Container.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 80%;
+  height: 100%;
+  margin: auto;
+  position: relative;
+}
+</style>
